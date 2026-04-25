00e89113363a1a33ac9cd7d20bcade536d24d25a
wuxx80 <wuxx80@qq.com>
2026-04-26 01:02:32 +0800

    修改替换 API 地址

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://118.24.164.85:21114".to_owned()
 }
 
 #[inline]
